0b8f2285c8a8f6095c300ee16e83e044c0727b17
Cristian Garcia <eminemko@hotmail.com>
2021-05-04 20:27:21 -0500

    Make loop to get 10 jokes and save them in the state

--- src/JokeList.js
+++ src/JokeList.js
@@ -3,11 +3,23 @@ import axios from "axios";
 
 
 class JokeList extends Component{
+  static defaultProps = {
+    numJokesToGet : 10
+  };
+
+  constructor(props){
+    super(props);
+    this.state = {jokes : []}
+  }
   async componentDidMount(){
     //Load Jokes
-    let res = await axios.get("https://icanhazdadjoke.com/", {
-      headers: {Accept: "application/json"}});
-    console.log(res.data.joke);
+    let jokes = [];
+    while(jokes.length < this.props.numJokesToGet){
+      let res = await axios.get("https://icanhazdadjoke.com/", 
+      { headers: {Accept: "application/json"} });
+      jokes.push(res.data.joke);
+    }
+    this.setState({ jokes: jokes });
   
   }
   render(){
