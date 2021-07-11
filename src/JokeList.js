3db4cea841e5dacc4101d1bc2f09625c112b0608
Cristian Garcia <eminemko@hotmail.com>
2021-07-11 11:51:13 -0500

    Enable new Jokes button, make the data persist

--- src/JokeList.js
+++ src/JokeList.js
@@ -12,12 +12,11 @@ class JokeList extends Component{
   constructor(props){
     super(props);
     this.state = { jokes : JSON.parse(window.localStorage.getItem("jokes") || "[]") };
+    this.handleClick = this.handleClick.bind(this);
   }
+ 
   componentDidMount(){
-    if(this.state.jokes.length === 0) this.getJokes();
-    //Load Jokes
-    
-  
+    if(this.state.jokes.length === 0) this.getJokes();  
   }
 
   async getJokes(){
@@ -27,16 +26,28 @@ class JokeList extends Component{
       { headers: {Accept: "application/json"} });
       jokes.push({id: uuidv4(), text: res.data.joke, votes: 0});
     }
-    this.setState({ jokes: jokes });
-    window.localStorage.setItem("jokes", JSON.stringify(jokes));
-  }
+
+    this.setState( 
+      st => ({
+      jokes: [...st.jokes, ...jokes]
+      }),
+      () => 
+        window.localStorage.setItem("jokes", JSON.stringify(this.state.jokes))
+        );
+    }
 
   handleVote(id, delta){
     this.setState(
       st => ({jokes: st.jokes.map(j => j.id === id ? {...j, votes: j.votes + delta} : j)
-      }) 
+      }),
+      () => window.localStorage.setItem("jokes", JSON.stringify(this.state.jokes))
       );
   };
+  
+  handleClick(){
+    this.getJokes();
+  }
+
   render(){
     return(
       <div className='JokeList'>
@@ -45,10 +56,10 @@ class JokeList extends Component{
             <span>Daddy</span> Jokes
           </h1>
           <img src='https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg' />
-          <button className='JokeList-getmore'>New Jokes</button>
+          <button className='JokeList-getmore' onClick={this.handleClick}>New Jokes</button>
         </div>
         <div className='JokeList-jokes'>
-          {
+          { 
             this.state.jokes.map(j => 
               ( <Joke
                 key={j.id} 
