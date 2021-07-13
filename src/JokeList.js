740b0766b86430c07b21fa1011a67a07803e22e9
Cristian Garcia <eminemko@hotmail.com>
2021-07-13 15:44:53 -0500

    Sort jokes according the number of votes

--- src/JokeList.js
+++ src/JokeList.js
@@ -70,6 +70,8 @@ class JokeList extends Component{
         </div>
       )
     }
+
+    let jokes = this.state.jokes.sort((a,b) => b.votes - a.votes);
     return(
       <div className='JokeList'>
         <div className='JokeList-sidebar'>
@@ -81,7 +83,7 @@ class JokeList extends Component{
         </div>
         <div className='JokeList-jokes'>
           { 
-            this.state.jokes.map(j => 
+            jokes.map(j => 
               ( <Joke
                 key={j.id} 
                 text={j.text} 
